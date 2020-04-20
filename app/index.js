960c86542c2ad69d796057216c908542e1bb0457
Gondwana Software <gondwanasoft@users.noreply.github.com>
2020-04-20 12:47:46 +1000

    Removed unnecessary code

--- app/index.js
+++ app/index.js
@@ -1,55 +1,21 @@
 import clock from "clock"
-import { memory } from "system"
-import { today, goals } from "user-activity"
 import { Gauge } from './gauge/gauge.js'
 
-console.log("mem 0 =" + (memory.js.used))
-const fatBurnGauge = new Gauge('fatBurnGauge', 'fatBurn.png')
-console.log("mem 1 =" + (memory.js.used))
-const cardioGauge = new Gauge('cardioGauge', 'cardio.png', true)
-console.log("mem 2 =" + (memory.js.used))
-const peakGauge = new Gauge('peakGauge', 'peak.png')
-console.log("mem 3 =" + (memory.js.used))
+const energyGauge = new Gauge('energyGauge', 'energy.png', true)
+const heartGauge = new Gauge('heartGauge', 'heart.png')
+const stepsGauge = new Gauge('stepsGauge', 'steps.png')
 
-fatBurnGauge.goal = 2
-cardioGauge.achiev = 2
-peakGauge.goal = 0
+energyGauge.goal = 2500
+heartGauge.goal = 120
+stepsGauge.goal = 10000
 
-fatBurnGauge.percent = true
-
-if (today.local.activeZoneMinutes) {
-  console.log(`Local:`);
-  console.log(`today.local.activeZoneMinutes.total=${today.local.activeZoneMinutes.total}`);
-  console.log(`today.local.activeZoneMinutes.fatBurn=${today.local.activeZoneMinutes.fatBurn}`);
-  console.log(`today.local.activeZoneMinutes.cardio=${today.local.activeZoneMinutes.cardio}`);
-  console.log(`today.local.activeZoneMinutes.peak=${today.local.activeZoneMinutes.peak}`);
-  console.log(`Adjusted:`);
-  console.log(`today.adjusted.activeZoneMinutes.total=${today.adjusted.activeZoneMinutes.total}`);
-  console.log(`today.adjusted.activeZoneMinutes.fatBurn=${today.adjusted.activeZoneMinutes.fatBurn}`);
-  console.log(`today.adjusted.activeZoneMinutes.cardio=${today.adjusted.activeZoneMinutes.cardio}`);
-  console.log(`today.adjusted.activeZoneMinutes.peak=${today.adjusted.activeZoneMinutes.peak}`);
-  console.log(`Goals:`);
-  console.log(`goals.activeZoneMinutes.total=${goals.activeZoneMinutes.total}`);
-  console.log(`goals.activeZoneMinutes.fatBurn=${goals.activeZoneMinutes.fatBurn}`);
-  console.log(`goals.activeZoneMinutes.cardio=${goals.activeZoneMinutes.cardio}`);
-  console.log(`goals.activeZoneMinutes.peak=${goals.activeZoneMinutes.peak}`);
-
-  fatBurnGauge.achiev = today.local.activeZoneMinutes.fatBurn
-  cardioGauge.achiev = today.local.activeZoneMinutes.cardio
-  peakGauge.achiev = today.local.activeZoneMinutes.peak
-}
-
-console.log(`achiev=1`)
-peakGauge.achiev = 1
-console.log(`achiev=0`)
-peakGauge.achiev = 0
+stepsGauge.percent = true   // setting percent using the property
 
 clock.granularity = "seconds"
 
 clock.ontick = function(evt) {
-  let sec = evt.date.getSeconds() % 5
-  //console.log(`sec=${sec}`);
-  fatBurnGauge.achiev = sec
-  cardioGauge.goal = sec
-  peakGauge.achiev = sec
+  let sec = evt.date.getSeconds() % 10  // gives values 0 to 9
+  energyGauge.achiev = sec * 600        // 0 to 5400
+  heartGauge.achiev = sec * 10 + 60     // 60 to 150
+  stepsGauge.achiev = sec * 3000        // 0 to 27000
 }
